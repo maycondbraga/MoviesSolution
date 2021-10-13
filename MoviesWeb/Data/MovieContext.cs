c81c70321a2ce79e372963b43fa2312cd74c5102
Maycon Douglas <maycondbraga.s@gmail.com>
2021-10-13 06:02:56 -0300

    Add project files.

---
+++ MoviesWeb/Data/MovieContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MoviesWeb.Models;
+
+namespace MoviesWeb.Data
+{
+    public class MovieContext : DbContext
+    {
+        public MovieContext(DbContextOptions<MovieContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Movie> Movie { get; set; }
+    }
+}
---
+++ MoviesWeb/Migrations/20211013051104_InitialCreateLocalDB.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MoviesWeb.Migrations
+{
+    public partial class InitialCreateLocalDB : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Price",
+                table: "Movie",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Price",
+                table: "Movie",
+                type: "decimal",
+                nullable: false,
+                oldClrType: typeof(decimal));
+        }
+    }
+}
